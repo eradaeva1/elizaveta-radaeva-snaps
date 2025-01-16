24bc97f528b65a774b612c42cc69421790f48fbe
Eradaeva1 <radaeva.liza98@gmail.com>
2025-01-16 04:45:56 -0500

    nav button edits

--- elizaveta-radaeva-snaps/src/components/Components/Header.jsx
+++ elizaveta-radaeva-snaps/src/components/Components/Header.jsx
@@ -1,16 +1,23 @@
 // import Header from './Header.scss';
 
 function Header() {
-    return (
-    
-         <header>
-         <nav className="nav">
-         <h2 className="nav__logo">Snaps</h2>
-         <button className="nav__button">Filter</button>
-         </nav>
-         </header>
-
-    );
+  return (
+    <header>
+      <nav className="nav">
+        <h2 className="nav__logo">Snaps</h2>
+        <div className="nav__right">
+          <button className="nav__button">
+            Filters
+            <img
+              src="src\assets\logos\Filter.svg"
+              alt="filter icon"
+              className="nav__button-icon"
+            />
+          </button>
+        </div>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
