31b32202983e3d822b65ff84a2d07e09490ae480
Eradaeva1 <radaeva.liza98@gmail.com>
2025-01-16 04:01:03 -0500

    header and hero styling add

--- elizaveta-radaeva-snaps/src/components/Components/Hero.jsx
+++ elizaveta-radaeva-snaps/src/components/Components/Hero.jsx
@@ -1,14 +1,15 @@
 // import Hero from './Hero.scss';
 
 function Hero() {
-    return (
-        
-        <section className="hero">
-<p className="hero__title">Our mission:</p>
-<h1 className="hero__title-text">Provide photographers a space to share photos of the neighborhoods they cherish, <span>expressed in their unique style.</span></h1>
-        </section>
-
-    );
+  return (
+    <section className="hero">
+      <p className="hero__title">Our mission:</p>
+      <h1 className="hero__title-text">
+        Provide photographers a space to share photos of the neighborhoods they
+        cherish, <span className="hero__title-text--special">expressed in their unique style.</span>
+      </h1>
+    </section>
+  );
 }
 
 export default Hero;
