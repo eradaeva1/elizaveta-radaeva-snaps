014dc51d316aedfccc502e77e51fd1107d81080e
Eradaeva1 <radaeva.liza98@gmail.com>
2025-01-16 03:01:32 -0500

    header and hero sections add

---
+++ elizaveta-radaeva-snaps/src/components/Components/Header.jsx
@@ -0,0 +1,16 @@
+// import Header from './Header.scss';
+
+function Header() {
+    return (
+    
+         <header>
+         <nav className="nav">
+         <h2 className="nav__logo">Snaps</h2>
+         <button className="nav__button">Filter</button>
+         </nav>
+         </header>
+
+    );
+}
+
+export default Header;
---
+++ elizaveta-radaeva-snaps/src/components/Components/Hero.jsx
@@ -0,0 +1,14 @@
+// import Hero from './Hero.scss';
+
+function Hero() {
+    return (
+        
+        <section className="hero">
+<p className="hero__title">Our mission:</p>
+<h1 className="hero__title-text">Provide photographers a space to share photos of the neighborhoods they cherish, <span>expressed in their unique style.</span></h1>
+        </section>
+
+    );
+}
+
+export default Hero;
