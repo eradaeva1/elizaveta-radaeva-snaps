f791dc67777b2f0c0e3b3dd84081d2e380e1f7c6
Eradaeva1 <radaeva.liza98@gmail.com>
2025-01-16 05:13:56 -0500

    footer add

--- elizaveta-radaeva-snaps/src/App.jsx
+++ elizaveta-radaeva-snaps/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.scss'
  import Header from './components/Components/Header'
   import Hero from './components/Components/Hero'
+  import Footer from './components/Components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
     <>
       <Header />
       <Hero />
+      <Footer />
     </>
   )
 }
--- elizaveta-radaeva-snaps/src/components/Components/Footer.jsx
+++ elizaveta-radaeva-snaps/src/components/Components/Footer.jsx
@@ -1,10 +1,11 @@
-import Footer from './Footer.scss';
+// import Footer from './Footer.scss';
 
 function Footer() {
     return (
-        <>
+        <section className="footer">
+        <h2 className="nav__logo">Snaps</h2>
 
-        </>
+        </section>
     );
 }
 
